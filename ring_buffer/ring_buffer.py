8380900683247e5d72a557caa256e88d5e11978d
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-09-20 13:43:44 -0400

    Get function returns a list that doesn't include 'None'

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,26 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+    # Adds elements to the buffer
+    def append(self, item):
+        return self.storage.append(item)
 
-  def get(self):
-    pass
+    # Returns all of the elements in the buffer in a list in their given order.
+    # Should not return any "None" values in the list even if they are present in the buffer
+    def get(self):
+        items = []
+        for item in self.storage:
+            print(item)
+            if item != None:
+                items.append(item)
+        return items
+
+
+buffer = RingBuffer(3)
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+print(buffer.get())
